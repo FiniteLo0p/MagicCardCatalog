872325086c0f558cf8b14b5ed14d3035f5df735f
Jason Smith <111402432+FiniteLo0p@users.noreply.github.com>
2022-08-18 18:21:10 -0500

    import tkinter and starting layout.

---
+++ main.py
@@ -0,0 +1,17 @@
+# Created by Jason Smith
+# Started August 18, 2022
+
+# Description: The purpose of this program is to store and display a catalog of
+# owned "Magic: The Gathering" cards in my collection.
+
+import tkinter.ttk as ttk
+from tkinter.ttk import *
+
+def main():
+    """"""
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    
